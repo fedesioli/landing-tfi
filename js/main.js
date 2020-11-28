3a9cfdcd1a03b0cfaae99215844de0d2d775fab9
Federico Sioli <fsioli@udesa.edu.ar>
2020-11-28 14:41:35 -0300

    feedback julito

--- js/main.js
+++ js/main.js
@@ -579,22 +579,22 @@
         "time": {
             "Days": {
                 "text": "Days",
-                "color": "#0038E3",
+                "color": "#E97500",
                 "show": true
             },
             "Hours": {
                 "text": "Hours",
-                "color": "#0038E3",
+                "color": "#E97500",
                 "show": true
             },
             "Minutes": {
                 "text": "Minutes",
-                "color": "#0038E3",
+                "color": "#E97500",
                 "show": true
             },
             "Seconds": {
                 "text": "Seconds",
-                "color": "#0038E3",
+                "color": "#E97500",
                 "show": true
             }
         }
